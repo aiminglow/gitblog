fd96a417fe384eca9d247621fff95da962fb2256
aiminglow <zhangchichichichi@gmail.com>
2019-12-24 00:17:16 +0800

    添加用户登录状态校验拦截器

--- src/main/java/com/aiminglow/gitblog/config/CustomWebMvcConfigurer.java
+++ src/main/java/com/aiminglow/gitblog/config/CustomWebMvcConfigurer.java
@@ -1,9 +1,24 @@
 package com.aiminglow.gitblog.config;
 
+import com.aiminglow.gitblog.interceptor.UserLoginInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 /**
  * @ClassName CustomWebMvcConfigurer
  * @Description TODO
  * @Author aiminglow
  */
-public class CustomWebMvcConfigurer {
+@Configuration
+public class CustomWebMvcConfigurer implements WebMvcConfigurer {
+
+    @Autowired
+    private UserLoginInterceptor userLoginInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(userLoginInterceptor).addPathPatterns("/management/**");
+    }
 }
--- src/main/java/com/aiminglow/gitblog/interceptor/UserLoginInterceptor.java
+++ src/main/java/com/aiminglow/gitblog/interceptor/UserLoginInterceptor.java
@@ -1,9 +1,38 @@
 package com.aiminglow.gitblog.interceptor;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 /**
  * @ClassName UserLoginInterceptor
- * @Description TODO
+ * @Description 用户登录状态校验 拦截器：如果未登录，导向登录页面
+ * TODO 之后还要做一个，如果已经登录，前往登录页面的话，直接导向用户主页或者其他页面
  * @Author aiminglow
  */
-public class UserLoginInterceptor {
+@Component
+public class UserLoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception{
+        HttpSession session = request.getSession();
+        String uri = request.getRequestURI();
+        if (uri.startsWith("/management") && session.getAttribute("userId") == null) {
+            response.sendRedirect(request.getContextPath() + "/user/login");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+    }
 }
